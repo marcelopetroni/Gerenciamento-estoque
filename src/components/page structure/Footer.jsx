1dea7e158ea212877d2953fdedb7c41814cfcaf9
marcelopetroni <marceloapetroni@hotmail.com>
2024-06-04 08:59:54 -0300

    header e footer

---
+++ src/components/page structure/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/Footer.sass';
+
+const Footer = () => {
+  return (
+    <footer>
+        <h4>Created by marcelopetroni - Front-end Developer</h4>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/page structure/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/Header.sass';
+import { RiShoppingCartLine } from "react-icons/ri";
+
+const Header = () => {
+  return (
+    <header>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet"></link>
+        <div className="icon" ><RiShoppingCartLine /></div>
+        <h1 className='logo-container' >Nunes Sports</h1>
+    </header>
+  )
+}
+
+export default Header
