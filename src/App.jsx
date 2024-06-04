5f926750fcf404f3045084224701b16e43eaadbf
marcelopetroni <marceloapetroni@hotmail.com>
2024-06-04 09:45:33 -0300

    exibição do conteúdo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import Header from './components/page structure/Header'
 import MainContent from './components/page structure/MainContent'
+import Footer from './components/page structure/Footer'
 
 function App() {
 
   return (
     <>
+      <Header/>
       <MainContent/>
+      <Footer />
     </>
   )
 }
